ded4072bb689469ad6feed4acec19a75f40bcbb2
coldfear457@gmail.com <coldfear457@gmail.com>
2021-08-06 22:07:16 +0600

    feat: add decision for typical-arrays-problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array<=0 || !array) {
+        return 0;
+    } else {
+        array.sort((a, b) => b - a);
+        array.reverse();
+        return array[0];
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array<=0 || !array) {
+        return 0;
+    } else {
+        array.sort((a, b) => b - a);
+        return array[0];
+    }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array<=0 || !array) {
+        return 0;
+    } else {
+        let len = array.length,
+            sum = 0;
+
+        for (let i = 0; i < len; i++) {
+            sum += array[i];
+        }
+
+        return sum / len;
+    }
 }
+
